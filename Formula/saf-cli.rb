fb07da32eac809fdb77f6d744c3475df192a722f
ChrisHinchey <112198118+ChrisHinchey@users.noreply.github.com>
2023-01-03 18:58:07 -0500

    Update saf-cli.rb

--- Formula/saf-cli.rb
+++ Formula/saf-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class SafCli < Formula
   desc "CLI for streamlining security automation of systems and DevOps pipelines"
   homepage "https://github.com/mitre/saf"
-  url "https://registry.npmjs.org/@mitre/saf/-/saf-1.2.0.tgz"
-  sha256 "d39c06230d3e1e10f012836e8911445b4ff354d2fb8a77caba10179b8884b02d"
+  url "https://registry.npmjs.org/@mitre/saf/-/saf-1.2.1.tgz"
+  sha256 "c38de9de34e3e6dbd65e2f8d678123cb54936e8e0f47e1c332969977f8349d0b"
   license "Apache-2.0"
 
   livecheck do
