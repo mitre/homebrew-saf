511733895765fae7afaa7ea43e874bc773290e48
Amndeep Singh Mann <me@asm.works>
2023-07-11 16:35:08 -0400

    saf-cli 1.2.22
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/saf-cli.rb
+++ Formula/saf-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class SafCli < Formula
   desc "CLI for streamlining security automation of systems and DevOps pipelines"
   homepage "https://github.com/mitre/saf"
-  url "https://registry.npmjs.org/@mitre/saf/-/saf-1.2.21.tgz"
-  sha256 "789e79210b332597702cb8978f43607b990c6bb3050b0e09d34feedb52ba7d9d"
+  url "https://registry.npmjs.org/@mitre/saf/-/saf-1.2.22.tgz"
+  sha256 "8780949bb6694b6512f3709531eb3d7ee12708963ffb40bc46cbc5821b6ca798"
   license "Apache-2.0"
 
   livecheck do
